c05c6d15f0404897e6465e7591c6b4bad51a5c92
I S L A M <islamashraf1539@gmail.com>
2024-01-25 05:46:11 +0200

    2- Add Photos to assets & Add in Constants File

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 const String kHomeImage = "assets/images/mosque home view.jpg";
+const String kCompassImage = "assets/images/compass.png";
+const String kNeedleImage = "assets/images/needle.png";
+const String kMeccahImage = "assets/images/Mecca.png";
+
 const Color kPrimaryColor = Color(0xff15a78e);
 const Color kCardColorOne = Color(0xff13a088);
 const Color kCardColorTwo = Color(0xffe78b5e);

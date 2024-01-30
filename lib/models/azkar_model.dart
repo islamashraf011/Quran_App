168487fd51758e601b80a0528d090ae971b374ab
I S L A M <islamashraf1539@gmail.com>
2024-01-30 16:28:53 +0200

    Add Azkar json file in assets & Create Azkar Model

---
+++ lib/models/azkar_model.dart
@@ -0,0 +1,19 @@
+class AzkarModel {
+  final String zekr;
+  final int repeat;
+  final String bless;
+
+  AzkarModel({
+    required this.zekr,
+    required this.repeat,
+    required this.bless,
+  });
+
+  factory AzkarModel.fromJson(json) {
+    return AzkarModel(
+      zekr: json["zekr"],
+      repeat: json["repeat"],
+      bless: json["bless"],
+    );
+  }
+}

5dcf6f57f63446936f358539a430343b88d14721
I S L A M <islamashraf1539@gmail.com>
2024-02-07 14:33:10 +0200

    Add jsonFilesofAzkar in assets&Put pathin constant

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const String kHomeImage = "assets/images/mosque home view.jpg";
 const String kCompassImage = "assets/images/compass.png";
@@ -10,3 +11,43 @@ const Color kCardColorOne = Color(0xff13a088);
 const Color kCardColorTwo = Color(0xffe78b5e);
 const Color kCardColorThree = Color(0xff1a84ad);
 const Color kCardColorFour = Color(0xffb05ee7);
+
+const Color kAzkarColor = Color(0xffffd143);
+
+const String kAzkarSabah = "assets/jsons/azkar_json/azkar_sabah.json";
+const String kAzkarMassa = "assets/jsons/azkar_json/azkar_massa.json";
+const String kAzkarAfterPray = "assets/jsons/azkar_json/azkar_after_pray.json";
+const String kAzkarBeforeSleeping =
+    "assets/jsons/azkar_json/azkar_before_sleep.json";
+const String kAzkarHearingAzan = "assets/jsons/azkar_json/azkar_hear_azan.json";
+const String kAzkarHome = "assets/jsons/azkar_json/azkar_home.json";
+const String kAzkarinPraying = "assets/jsons/azkar_json/azkar_in_praying.json";
+const String kAzkarMecca = "assets/jsons/azkar_json/azkar_mecca.json";
+const String kAzkarWakingUp = "assets/jsons/azkar_json/azkar_wake_up.json";
+const String kAzkarWashing = "assets/jsons/azkar_json/azkar_washing.json";
+
+const List<String> names = [
+  "أذكـــار الصبـــاح",
+  "أذكـــار المســـاء",
+  "أذكـــار بعد الصــلاة",
+  "أذكـــار النــوم",
+  "أذكـــار الاستيقاظ",
+  "أذكـــار أثناء الصـلاة",
+  "أذكـــار الآذان",
+  "أذكـــار الوضـوء",
+  "أذكـــار المنزل",
+  "أذكــار الحـج والعمـرة",
+];
+
+const List<IconData> icon = [
+  Icons.wb_sunny_outlined,
+  FontAwesomeIcons.moon,
+  FontAwesomeIcons.checkDouble,
+  Icons.night_shelter_outlined,
+  Icons.alarm,
+  FontAwesomeIcons.personPraying,
+  Icons.mosque_outlined,
+  Icons.wash_outlined,
+  Icons.home_outlined,
+  FontAwesomeIcons.handsPraying
+];

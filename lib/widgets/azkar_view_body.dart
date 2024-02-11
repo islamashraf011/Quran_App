ec5590a0d70714884b7fff2e558e0d94576d8310
I S L A M <islamashraf1539@gmail.com>
2024-02-11 23:35:48 +0200

    Use CustomListViewNames&Function in AzkarViewBody

--- lib/widgets/azkar_view_body.dart
+++ lib/widgets/azkar_view_body.dart
@@ -1,26 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/constants/constants.dart';
-import 'package:quran_app/widgets/custom_button_widget.dart';
+import '../services/represent_azkar_function.dart';
+import 'custom_listview_of_names.dart';
 
 class AzkarViewBody extends StatelessWidget {
   const AzkarViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: names.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 20,
-          ),
-          child: CustomButton(
-            title: names[index],
-            icon: icon[index],
-            iconSize: MediaQuery.of(context).size.width * 0.12,
-            onTap: () {},
-          ),
-        );
+    return CustomListViewofNames(
+      itemCount: namesofAzkar.length,
+      title: namesofAzkar,
+      iconData: icon,
+      onTap: (index) {
+        representDataFunction(index, context);
       },
     );
   }

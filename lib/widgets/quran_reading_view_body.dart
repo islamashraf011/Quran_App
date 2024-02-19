d30a08d2b18accdb273352be3b44d90f24e99ddb
I S L A M <islamashraf1539@gmail.com>
2024-02-20 00:33:40 +0200

    Used PageViewBuilder to Represent Quranimage in Ui

--- lib/widgets/quran_reading_view_body.dart
+++ lib/widgets/quran_reading_view_body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import '../services/get_quran_img_path.dart';
 
 class QuranReadingViewBody extends StatelessWidget {
   const QuranReadingViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Recieve Data (Path) from function in this Variable
+    final List<String> quranList = generateFilePaths();
+    return PageView.builder(
+      itemCount: quranList.length,
+      reverse: true,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        // To Make Zoom on images used this Widget interActiveViewer
+        return InteractiveViewer(
+          maxScale: 4,
+          minScale: 0.5,
+          child: Image.asset(
+            quranList[index],
+            fit: BoxFit.fill,
+          ),
+        );
+      },
+    );
   }
 }

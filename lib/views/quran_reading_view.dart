75ce388191e9e8e38af8c10b4011958622e64122
I S L A M <islamashraf1539@gmail.com>
2024-02-20 04:03:34 +0200

    Used Custom Quran AppBar in Quran Reading View

--- lib/views/quran_reading_view.dart
+++ lib/views/quran_reading_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../components/custom_row_menu_item.dart';
+import '../components/custom_quran_app_bar.dart';
 import '../widgets/quran_reading_view_body.dart';
 
 class QuranReadingView extends StatefulWidget {
@@ -25,78 +25,37 @@ class _QuranReadingViewState extends State<QuranReadingView> {
       },
       child: Scaffold(
         backgroundColor: Colors.blue[300],
-        appBar: showAppBar
-            ? AppBar(
-                backgroundColor: Colors.black.withOpacity(0.7),
-                actions: [
-                  const Text(
-                    "القائمة الرئيسية",
-                    textDirection: TextDirection.rtl,
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  PopupMenuButton(
-                    color: Colors.black.withOpacity(0.6),
-                    icon: const Icon(Icons.menu),
-                    offset: const Offset(0, 55),
-                    itemBuilder: (context) {
-                      // Make this step to handle it to take Divider
-                      return <PopupMenuEntry<dynamic>>[
-                        PopupMenuItem(
-                          child: const CustomRowMenuItem(
-                            icon: Icons.more_vert,
-                            text: "الفهـرس",
-                          ),
-                          onTap: () {},
-                        ),
-                        const PopupMenuDivider(
-                          height: 20,
-                        ),
-                        PopupMenuItem(
-                          child: const CustomRowMenuItem(
-                            icon: Icons.bookmark_add_outlined,
-                            text: "حفظ علامـة",
-                          ),
-                          onTap: () {
-                            setState(() {
-                              //Make markedPageIndex = current index in PageViewBuilder to put BookMark on It
-                              markedPageIndex = pageController.page!.round();
-                            });
-                          },
-                        ),
-                        const PopupMenuDivider(
-                          height: 20,
-                        ),
-                        PopupMenuItem(
-                          child: const CustomRowMenuItem(
-                            icon: Icons.bookmark_border,
-                            text: "انتقال إلي العلامـة",
-                          ),
-                          onTap: () {
-                            //Use PageController to go to the CurrentIndex that has the BookMark
-                            pageController.animateToPage(
-                              markedPageIndex,
-                              duration: const Duration(seconds: 1),
-                              curve: Curves.easeIn,
-                            );
-                          },
-                        ),
-                      ];
-                    },
-                  ),
-                ],
-              )
-            : null,
-        body: QuranReadingViewBody(
-          pageController: pageController,
-          markedPageIndex: markedPageIndex,
-          onBookmarkTap: (index) {
-            setState(() {
-              markedPageIndex = index;
-            });
-          },
+        body: Column(
+          children: [
+            if (showAppBar)
+              CustomQuranAppBar(
+                addMark: () {
+                  setState(() {
+                    //Make markedPageIndex = current index in PageViewBuilder to put BookMark on It
+                    markedPageIndex = pageController.page!.round();
+                  });
+                },
+                goToMark: () {
+                  // Use PageController to go to the CurrentIndex that has the BookMark
+                  pageController.animateToPage(
+                    markedPageIndex,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.easeIn,
+                  );
+                },
+              ),
+            Expanded(
+              child: QuranReadingViewBody(
+                pageController: pageController,
+                markedPageIndex: markedPageIndex,
+                onBookmarkTap: (index) {
+                  setState(() {
+                    markedPageIndex = index;
+                  });
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );

cc9d6f2a34e3af424bd9fadc4c19d6b14c65754c
I S L A M <islamashraf1539@gmail.com>
2024-02-20 07:18:46 +0200

    UsedRefernce in QuranReadingView toHandle MarkPage

--- lib/views/quran_reading_view.dart
+++ lib/views/quran_reading_view.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/services/shared_prefrence_service.dart';
 import '../components/custom_quran_app_bar.dart';
 import '../widgets/quran_reading_view_body.dart';
 
 class QuranReadingView extends StatefulWidget {
   const QuranReadingView({super.key});
   static String id = "QuranReading";
-
   @override
   State<QuranReadingView> createState() => _QuranReadingViewState();
 }
 
 class _QuranReadingViewState extends State<QuranReadingView> {
   bool showAppBar = false;
-  int markedPageIndex = 0; //Default Value that make mark on first page
+  int? markedPageIndex; //Default Value that make mark on first page
   PageController pageController = PageController();
+
+  @override
+  void initState() {
+    loadMarkedPageIndex();
+    super.initState();
+  }
+
+  void loadMarkedPageIndex() async {
+    int lastIndex = await MarkedPage().loadMarkedPageIndex();
+    setState(() {
+      markedPageIndex = lastIndex;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,16 +42,17 @@ class _QuranReadingViewState extends State<QuranReadingView> {
           children: [
             if (showAppBar)
               CustomQuranAppBar(
-                addMark: () {
+                addMark: () async {
                   setState(() {
                     //Make markedPageIndex = current index in PageViewBuilder to put BookMark on It
                     markedPageIndex = pageController.page!.round();
                   });
+                  await MarkedPage().saveMarkedPageIndex(markedPageIndex!);
                 },
                 goToMark: () {
                   // Use PageController to go to the CurrentIndex that has the BookMark
                   pageController.animateToPage(
-                    markedPageIndex,
+                    markedPageIndex!,
                     duration: const Duration(seconds: 1),
                     curve: Curves.easeIn,
                   );
@@ -47,7 +61,7 @@ class _QuranReadingViewState extends State<QuranReadingView> {
             Expanded(
               child: QuranReadingViewBody(
                 pageController: pageController,
-                markedPageIndex: markedPageIndex,
+                markedPageIndex: markedPageIndex ?? 0,
                 onBookmarkTap: (index) {
                   setState(() {
                     markedPageIndex = index;

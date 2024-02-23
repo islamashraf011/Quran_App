566613457c0f8ff3d9ff032bd38dbe4e43c9b95d
I S L A M <islamashraf1539@gmail.com>
2024-02-24 01:58:47 +0200

    Add ListViewSeperated & SurahButton in QuranListen

--- lib/widgets/quran_listening_view_body.dart
+++ lib/widgets/quran_listening_view_body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/components/custom_surah_listening_button.dart';
+import 'package:quran_app/constants/constants.dart';
 
 class QuranListeningViewBody extends StatelessWidget {
   const QuranListeningViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //Used List View Seperated to Add Divider between item
+    return ListView.separated(
+      itemCount: surahList.length,
+      separatorBuilder: (context, index) => Divider(
+        height: MediaQuery.of(context).size.height * 0.001,
+      ),
+      itemBuilder: (context, index) {
+        return CustomSurahButton(
+          onTap: () {},
+          numberSurah: index,
+          nameSurah: surahList[index],
+        );
+      },
+    );
   }
 }

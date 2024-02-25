6861e3403475f0bac9abc6eb54dce89df72640f8
I S L A M <islamashraf1539@gmail.com>
2024-02-25 06:37:15 +0200

    Create Audio Player View & Add AppBar with Leading

---
+++ lib/views/audio_player_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../widgets/audio_player_view_body.dart';
+
+class AudioPlayerView extends StatelessWidget {
+  const AudioPlayerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xff071a2c),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.keyboard_arrow_down_rounded,
+            size: MediaQuery.of(context).size.height * 0.04,
+          ),
+        ),
+      ),
+      body: const AudioPlayerViewBody(),
+    );
+  }
+}

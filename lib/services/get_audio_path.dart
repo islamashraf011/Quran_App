173fc4f21076dd9c846115a2177e098a44796ba7
I S L A M <islamashraf1539@gmail.com>
2024-02-26 22:18:12 +0200

    Modify Get Audio Path Function

--- lib/services/get_audio_path.dart
+++ lib/services/get_audio_path.dart
@@ -1,7 +1,7 @@
 List<String> generateAudioPaths() {
   final List<String> audioFilePath = [];
   for (int i = 0; i <= 114; i++) {
-    final String formattedNumber = i.toString().padLeft(4, '0');
+    final String formattedNumber = i.toString().padLeft(3, '0');
     final String filePath = 'assets/audios/$formattedNumber.lite.mp3';
     audioFilePath.add(filePath);
   }

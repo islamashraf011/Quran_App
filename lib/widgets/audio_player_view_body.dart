1701a73ce170c0eda21aff941e89a6453583dd38
I S L A M <islamashraf1539@gmail.com>
2024-03-16 01:45:45 +0200

    Used AudioPlayerCubit in AudioPlayerViewBody

--- lib/widgets/audio_player_view_body.dart
+++ lib/widgets/audio_player_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quran_app/cubits/audio_player/audio_player_cubit.dart';
 import '../components/custom_play_audio.dart';
 import '../constants/constants.dart';
 
@@ -11,26 +13,47 @@ class AudioPlayerViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            kAudioColorOne,
-            kAudioColorTwo,
-          ],
-        ),
-      ),
-      child: CustomPlayAudio(
-        index: index,
-        sliderMax: 100,
-        audioRateValue: 0.2,
-        skipPrevious: () {},
-        playAudio: () {},
-        skipNext: () {},
+    return BlocProvider(
+      create: (context) => AudioPlayerCubit(index),
+      child: BlocBuilder<AudioPlayerCubit, AudioPlayerState>(
+        builder: (context, state) {
+          final cubit = BlocProvider.of<AudioPlayerCubit>(context);
+          final mediaQuery = MediaQuery.of(context).size;
+          final index = cubit.currentIndex;
+          bool isPlaying = cubit.isPlaying;
+          Duration maxDuration = cubit.duration ?? Duration.zero;
+          Duration position = cubit.position ?? Duration.zero;
+
+          return Container(
+            width: mediaQuery.width,
+            height: mediaQuery.height,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  kAudioColorOne,
+                  kAudioColorTwo,
+                ],
+              ),
+            ),
+            child: CustomPlayAudio(
+              pause: !isPlaying,
+              title: surahList[index],
+              maxDuration: maxDuration,
+              position: position,
+              playAudio: () {
+                cubit.play(index);
+              },
+              skipPrevious: () {
+                cubit.skipPrevios();
+              },
+              skipNext: () {
+                cubit.skipNext();
+              },
+            ),
+          );
+        },
       ),
     );
   }

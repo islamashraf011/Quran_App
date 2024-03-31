51ab2f2c930675ac79dbb463d8323fd38c41b541
I S L A M <islamashraf1539@gmail.com>
2024-03-31 06:38:23 +0200

    Modify Audio Path Function again with justAudio

--- lib/services/get_audio_path.dart
+++ lib/services/get_audio_path.dart
@@ -2,7 +2,7 @@ List<String> generateAudioPaths() {
   final List<String> audioFilePath = [];
   for (int i = 0; i <= 114; i++) {
     final String formattedNumber = i.toString().padLeft(3, '0');
-    final String filePath = 'audios/$formattedNumber.lite.mp3';
+    final String filePath = 'assets/audios/$formattedNumber.lite.mp3';
     audioFilePath.add(filePath);
   }
   return audioFilePath;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,8 @@ import Foundation
 
 import audio_service
 import audio_session
-import audioplayers_darwin
 import geolocator_apple
+import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -16,8 +16,8 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))

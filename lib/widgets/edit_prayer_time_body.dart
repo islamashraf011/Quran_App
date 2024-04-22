b12a2450f28b0736de64072baadd3bf2139793f3
I S L A M <islamashraf1539@gmail.com>
2024-04-22 05:48:31 +0200

    Modify PrayerTimeBody to Show PrayTime by Location

--- lib/widgets/edit_prayer_time_body.dart
+++ lib/widgets/edit_prayer_time_body.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../cubits/pray_timer_cubit/pray_timer_cubit.dart';
+import 'package:quran_app/components/custom_button_widget.dart';
+import 'package:quran_app/components/show_snack_bar.dart';
+import 'package:quran_app/constants/constants.dart';
+import 'package:quran_app/cubits/pray_timer_cubit/pray_timer_cubit.dart';
+import 'package:restart_app/restart_app.dart';
 import 'edit_time_card.dart';
 
 class EditPrayerTimeBody extends StatelessWidget {
-  const EditPrayerTimeBody({
-    super.key,
-  });
+  const EditPrayerTimeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    PrayTimerCubit cubit = BlocProvider.of<PrayTimerCubit>(context);
-    List<DateTime> listPrayerTime = cubit.getCurrentPrayerTime();
-    List<String> listPrayerName = cubit.prayerNames;
+    return BlocBuilder<PrayTimerCubit, PrayTimerState>(
+      builder: (context, state) {
+        PrayTimerCubit cubit = BlocProvider.of<PrayTimerCubit>(context);
+        List<DateTime> listPrayerTime = cubit.listofPrayTime;
 
-    return ListView.builder(
-      padding: EdgeInsets.symmetric(
-        vertical: MediaQuery.of(context).size.height * 0.03,
-        horizontal: MediaQuery.of(context).size.height * 0.01,
-      ),
-      itemCount: listPrayerTime.length,
-      itemBuilder: (context, index) {
-        return EditTimeCard(
-          index: index,
-          listPrayerName: listPrayerName,
-          listPrayerTime: listPrayerTime,
+        return Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.03,
+            horizontal: MediaQuery.of(context).size.height * 0.01,
+          ),
+          child: Column(
+            children: [
+              CustomButton(
+                title: "إضافة مواقيت الصلاة",
+                buttonColor: kCardColorOne,
+                icon: Icons.add,
+                iconSize: MediaQuery.of(context).size.height * 0.04,
+                onTap: () async {
+                  await cubit.addPrayerTimes(context);
+                  if (context.mounted) {
+                    showSnackBar(
+                      context,
+                      'سيتم إعادة تشغيل التطبيق',
+                    );
+                  }
+                  await Future.delayed(
+                    const Duration(seconds: 2),
+                  );
+                  Restart.restartApp();
+                },
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              if (state is PrayTimerload)
+                const Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              if (state is PrayTimerSuccessful)
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: listPrayerTime.length,
+                    itemBuilder: (context, index) {
+                      return EditTimeCard(
+                        index: index,
+                        listPrayerName: listPrayerName,
+                        listPrayerTime: listPrayerTime,
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
         );
       },
     );
